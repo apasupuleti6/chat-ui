e595df541e98c8cb00c0e08458f05de693a30cfb
Aswini Pasupuleti <aswinipasupuleti2@gmail.com>
2024-02-27 11:16:08 -0600

    adding basic code for chat

--- chat-ui/src/App.js
+++ chat-ui/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState('');
+  const webSocketURL = 'wss://jra5lrv0ne.execute-api.us-west-1.amazonaws.com/websocket-tf-dev-stage/';
+
+  useEffect(() => {
+    const ws = new WebSocket(webSocketURL);
+
+    ws.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket closed');
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  const handleMessageSend = () => {
+    const ws = new WebSocket(webSocketURL);
+    ws.onopen = () => {
+      ws.send(inputMessage);
+      setInputMessage('');
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WebSocket Chat App</h1>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={inputMessage}
+        onChange={(e) => setInputMessage(e.target.value)}
+      />
+      <button onClick={handleMessageSend}>Send</button>
     </div>
   );
 }
